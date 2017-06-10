244794d3133063589a54eb13013befc1ac4d8e33
sandy <sduxbury@gmail.com>
2017-06-10 11:32:15 -0600

    complete db schema and read/write

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -3,24 +3,48 @@
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
-import psycopg2
+import psycopg2 as psql
 
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    return psql.connect("dbname=tournament")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("delete from matches")  
+    db.commit()
 
+    #remove matches,wins,losses from players table
+    sql = ("""update players
+    set wins = %s, losses = %s, matches = %s;""")
+    c.execute(sql,(0,0,0))
+    print(' //// Test Remove Scores from Players /////')
+    playerStandings()
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("delete from players")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute("select count(*) as num_players from players")
+    n_players = c.fetchall()[0][0]
+    print('Number of players: ',n_players)
+    db.close()
+    return n_players
 
 
 def registerPlayer(name):
@@ -31,7 +55,17 @@ def registerPlayer(name):
   
     Args:
       name: the player's full name (need not be unique).
-    """
+    """   
+    db = connect()
+    c = db.cursor()
+    sql = ("""insert into players (fullname, wins, losses, matches)
+        values (%s,%s,%s,%s);""")
+        
+    c.execute(sql,(name,0,0,0))
+    #c.execute("insert into players (fullname,wins,losses,matches) values ('%s','%s','%s')" %(name,0,0))
+    db.commit()
+    db.close()
+
 
 
 def playerStandings():
@@ -47,6 +81,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    c = db.cursor()
+    c.execute("select id, fullname, wins, matches from players order by wins desc")
+    standings = c.fetchall()
+    db.close()
+    return standings
 
 
 def reportMatch(winner, loser):
@@ -56,6 +96,29 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    # update matches table
+    db = connect()
+    c = db.cursor()
+    sql = ('insert into matches (winner_id, loser_id)'
+        'values (%s,%s);')
+    c.execute(sql,(winner,loser))
+    db.commit()
+
+    # update wins and match count
+    sql = ("""update players
+        set wins = wins+%s, matches = matches+%s
+        where id = %s;""")
+    c.execute(sql, (1,1,winner))
+    db.commit()
+
+    # update losses and match count
+    sql = ("""update players
+        set losses = losses+%s, matches = losses+%s
+        where id = %s;""")
+    c.execute(sql,(1,1,loser))
+    db.commit()
+
+    db.close()
  
  
 def swissPairings():
@@ -73,5 +136,20 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    
+    standings = playerStandings()
+    print('/// QC Swiss Pair ///')
+    # standings is sorted by wins descending
+    # get every other row starting from zero
+    set_a = standings[::2]
+    # get every other row starting from 1
+    set_b = standings[1::2]
+    # get only the first 2 items from the nested lists (id, name)
+    plr_a = [set_a[i][0:2] for i in range(len(set_a))]
+    plr_b = [set_b[i][0:2] for i in range(len(set_b))]
 
+    # create new list with zip and list comprehension
+    pairs = [a+b for a,b in zip(plr_a,plr_b)]
+    db.close()
+    return pairs
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,32 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
 
+CREATE TABLE players (
+	id serial,
+	fullname varchar,
+	wins int,
+	losses int,
+	matches int,
+	primary key (id)
+);
+
+CREATE TABLE matches(
+	id serial,
+	player_a_id int,
+	player_b_id int,
+	player_a_name varchar,
+	player_b_name varchar,
+	winner_id int,
+	loser_id int,
+	player_a_score int,
+	player_b_score int,
+	primary key (id),
+	foreign key (player_a_id) references players(id),
+	foreign key (player_b_id) references players(id),
+	foreign key (winner_id) references players(id),
+	foreign key (loser_id) references players(id)
+);
